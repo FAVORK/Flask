f9d60609997320fb790ce753fadd0bf87d1a30dd
FAVORK <53710230+FAVORK@users.noreply.github.com>
2019-11-09 19:27:10 +0300

    Basic flask hello app

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/profile/<int:id>')
+def profile(id):
+	return 'This profile belongs to %d' % id
+
+
+if __name__ == '__main__':
+	app.run()
